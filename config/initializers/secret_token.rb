6a4aea9f05f772a67d0645bd41443d105c08f2ee
Kevin <diasks2@gmail.com>
2013-10-22 20:34:34 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsWebsocketsChatDemo::Application.config.secret_key_base = '92e7fa3474696f3354eb18c5d9d80132913885c981a80df12f49ee71bbeb22dbc4241ef7e4007816ffe8bca58e4b7a83cd2975e99ab87359ad4e89dc7344028f'
